8cf2ba55f2004f155477c57b4fc3ab5353703015
Anton Matosov <anton.matosov@gmail.com>
2024-07-23 21:09:34 -0700

    Migrate to new API EnvironmentVariableCollection that properly works on all shells and OS
    https://github.com/microsoft/vscode/issues/179476 Introduced a new API for EnvironmentVariableCollection that properly handles shell integrations, including login always shells on macOS
    
    Update `@types/vscode` and `@vscode/test-electron` to the latest versions to allow use of the new API
    Update `activateTerminal` to use new API:
     - use `applyAtShellIntegration: true` only for `PATH` variable because `info.vars` contains entrire environment, including read only variables like `PWD`, `SHLVL` and `_` which will error out.
     TODO: these read only vars and maybe some others should be cleaned up before replacing them everywhere

--- src/activations/activateTerminal.ts
+++ src/activations/activateTerminal.ts
@@ -2,22 +2,24 @@ import { pathKey } from '../infra'
 import { Observable } from 'rxjs'
 import { DisposableLike } from '../_definitions'
 import { delimiter } from 'path'
-import { ExtensionContext } from 'vscode'
+import { EnvironmentVariableMutatorOptions, ExtensionContext } from 'vscode'
 import { EnvironmentInfo } from '../micromamba'
 
 export function activateTerminal(info$: Observable<EnvironmentInfo>, ctx: ExtensionContext): DisposableLike {
+  const processCreationOnlyOption = { applyAtProcessCreation: true, applyAtShellIntegration: false } as EnvironmentVariableMutatorOptions
+  const forceAtShellIntegrationOption = { applyAtProcessCreation: true, applyAtShellIntegration: true } as EnvironmentVariableMutatorOptions
   const { environmentVariableCollection: col } = ctx
   const sub = info$.subscribe((info) => {
     col.clear()
     col.persistent = false
     if (info.ok) {
-      info.vars.forEach((v) => col.replace(v.name, v.value))
+      info.vars.forEach((v) => col.replace(v.name, v.value, v.name === pathKey ? forceAtShellIntegrationOption : processCreationOnlyOption))
     } else {
       const { mambaRootPrefix, mambaExe } = info.params.micromambaParams
       const pathPrependValue = `${mambaRootPrefix}${delimiter}`
-      col.prepend(pathKey, pathPrependValue)
-      col.replace('MAMBA_ROOT_PREFIX', mambaRootPrefix)
-      col.replace('MAMBA_EXE', mambaExe)
+      col.prepend(pathKey, pathPrependValue, forceAtShellIntegrationOption)
+      col.replace('MAMBA_ROOT_PREFIX', mambaRootPrefix, forceAtShellIntegrationOption)
+      col.replace('MAMBA_EXE', mambaExe, forceAtShellIntegrationOption)
     }
   })
   return { dispose: () => sub.unsubscribe() }
