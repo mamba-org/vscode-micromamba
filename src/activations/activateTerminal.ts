9b4368980034751532f5cb61d1d1fba058f44aaf
Anton Matosov <anton.matosov@gmail.com>
2024-07-24 16:59:54 -0700

    Add cleanup of default terminal and vscode variables upon import to avoid conficts upon propagation to new shell and subprocesses

--- src/activations/activateTerminal.ts
+++ src/activations/activateTerminal.ts
@@ -6,20 +6,19 @@ import { EnvironmentVariableMutatorOptions, ExtensionContext } from 'vscode'
 import { EnvironmentInfo } from '../micromamba'
 
 export function activateTerminal(info$: Observable<EnvironmentInfo>, ctx: ExtensionContext): DisposableLike {
-  const processCreationOnlyOption = { applyAtProcessCreation: true, applyAtShellIntegration: false } as EnvironmentVariableMutatorOptions
-  const forceAtShellIntegrationOption = { applyAtProcessCreation: true, applyAtShellIntegration: true } as EnvironmentVariableMutatorOptions
+  const options = { applyAtProcessCreation: true, applyAtShellIntegration: true } as EnvironmentVariableMutatorOptions
   const { environmentVariableCollection: col } = ctx
   const sub = info$.subscribe((info) => {
     col.clear()
     col.persistent = false
     if (info.ok) {
-      info.vars.forEach((v) => col.replace(v.name, v.value, v.name === pathKey ? forceAtShellIntegrationOption : processCreationOnlyOption))
+      info.vars.forEach((v) => col.replace(v.name, v.value, options))
     } else {
       const { mambaRootPrefix, mambaExe } = info.params.micromambaParams
       const pathPrependValue = `${mambaRootPrefix}${delimiter}`
-      col.prepend(pathKey, pathPrependValue, forceAtShellIntegrationOption)
-      col.replace('MAMBA_ROOT_PREFIX', mambaRootPrefix, forceAtShellIntegrationOption)
-      col.replace('MAMBA_EXE', mambaExe, forceAtShellIntegrationOption)
+      col.prepend(pathKey, pathPrependValue, options)
+      col.replace('MAMBA_ROOT_PREFIX', mambaRootPrefix, options)
+      col.replace('MAMBA_EXE', mambaExe, options)
     }
   })
   return { dispose: () => sub.unsubscribe() }
